c23fdbdfe8c2835481c1d8ec765b8135c82fec39
ayeankit <ankitkumar36918@gmail.com>
2025-07-10 16:01:01 +0530

    feat: add friendly root route to backend API

--- skill-spark-reporting-hub-main/backend/server.js
+++ skill-spark-reporting-hub-main/backend/server.js
@@ -3,4 +3,8 @@ const { port } = require('./config');
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Skill Spark Reporting Hub API is running!');
 }); 
